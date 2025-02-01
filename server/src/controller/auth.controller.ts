9dab1f54a6785fc345e2d9e7c00c4e06372214eb
rahul-dev7x <rahulbera9932@gmail.com>
2025-02-01 20:43:04 +0530

    Add JWT authentication and token generation for sign-in and sign-up routes

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+
+const generateToken=(email:string)=>{
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined");
+    }
+    const token=jwt.sign({email},process.env.JWT_SECRET,{expiresIn:"1h"})
+    return token;
+}
+
+
 
 
 export const signUp=async(req:Request,res:Response):Promise<void>=>{
@@ -37,6 +49,40 @@ export const signUp=async(req:Request,res:Response):Promise<void>=>{
 
 export const signIn=async(req:Request,res:Response):Promise<void>=>{
     try{
+        const {email,password}=req.body;
+        if(!email || !password)
+        {
+            res.status(401).json({message:"Please Fill all the field",success:false,error:true})
+            return
+        }
+        const user=await User.findOne({email});
+        if(!user)
+        {
+            res.status(400).json({message:"User Not Found",success:false,error:true})
+            return
+
+        }
+        const passwordCorrect=await bcrypt.compare(password,user.password!);
+        if(!passwordCorrect)
+        {
+            res.status(400).json({message:"Password Not Correct",success:false,error:true})
+            return 
+
+        }
+       
+        const token=generateToken(email);
+        //console.log(token)
+        res.cookie("netflix-token",token,{
+            httpOnly:true,
+            
+            secure:process.env.NODE_ENV==="production",
+            sameSite:"strict",
+            maxAge:60*60*60*60*1000
+
+        }).status(200).json({message:"Login Success",success:true,error:false,data:{
+            username:user.username,
+            email:user.email
+        }})
 
     }
     catch(error)
@@ -49,6 +95,8 @@ export const signIn=async(req:Request,res:Response):Promise<void>=>{
 
 export const logOut=async(req:Request,res:Response):Promise<void>=>{
     try{
+        res.clearCookie("netflix-token")
+        res.status(200).json({message:"Successfully logout",success:true,error:false})
 
     }
     catch(error)
